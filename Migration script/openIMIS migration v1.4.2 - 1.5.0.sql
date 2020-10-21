8d19ecb572f6ed4e88ce2da048d207af8a110cdf
Dragos Dobre <dragos.dobre@swisstph.ch>
2020-10-21 15:54:38 +0200

    Fix uspSSRSCapitationPayment

--- Migration script/openIMIS migration v1.4.2 - 1.5.0.sql
+++ Migration script/openIMIS migration v1.4.2 - 1.5.0.sql
@@ -1372,24 +1372,55 @@ BEGIN
 			    @WeightNumberVisits DECIMAL(5, 2),
 			    @WeightAdjustedAmount DECIMAL(5, 2)
 
+
 	    DECLARE @FirstDay DATE = CAST(@Year AS VARCHAR(4)) + '-' + CAST(@Month AS VARCHAR(2)) + '-01'; 
 	    DECLARE @LastDay DATE = EOMONTH(CAST(@Year AS VARCHAR(4)) + '-' + CAST(@Month AS VARCHAR(2)) + '-01', 0)
 	    DECLARE @DaysInMonth INT = DATEDIFF(DAY,@FirstDay,DATEADD(MONTH,1,@FirstDay));
 
 		set @DistrictId = CASE @DistrictId WHEN 0 THEN NULL ELSE @DistrictId END
 
+		DECLARE @Locations TABLE (
+			LocationId INT,
+			LocationName VARCHAR(50),
+			LocationCode VARCHAR(8),
+			ParentLocationId INT
+			);
+	    
+		INSERT INTO @Locations 
+		    SELECT 0 LocationId, N'National' LocationName, NULL ParentLocationId,  0 LocationCode
+		    
+			UNION ALL
+		    
+			SELECT LocationId,LocationName, LocationCode, ISNULL(ParentLocationId, 0) 
+			FROM tblLocations 
+			WHERE (ValidityTo IS NULL )
+				AND (LocationId = ISNULL(@DistrictId, @RegionId) OR 
+				(LocationType IN ('R', 'D') AND ParentLocationId = ISNULL(@DistrictId, @RegionId)))
+		    
+			/*UNION ALL
+		    
+			SELECT L.LocationId, L.LocationName, L.ParentLocationId 
+		    FROM tblLocations L 
+		    INNER JOIN @Locations LC ON @LC.LocationId = L.ParentLocationId
+		    WHERE L.validityTo IS NULL
+		    AND L.LocationType IN ('R', 'D')*/
+		
 		DECLARE @LocationTemp table (LocationId int, RegionId int, RegionCode [nvarchar](8) , RegionName [nvarchar](50), DistrictId int, DistrictCode [nvarchar](8), 
-			DistrictName [nvarchar](50), WardId int, WardCode [nvarchar](8), WardName [nvarchar](50), VillageId int, VillageCode [nvarchar](8), VillageName [nvarchar](50), ParentLocationId int)
+			DistrictName [nvarchar](50), ParentLocationId int)
+		
 
 		INSERT INTO  @LocationTemp(LocationId , RegionId , RegionCode , RegionName , DistrictId , DistrictCode , 
-		DistrictName , WardId , WardCode , WardName , VillageId , VillageCode , VillageName , ParentLocationId) SELECT LocationId , RegionId , RegionCode  , RegionName , DistrictId , DistrictCode , 
-		DistrictName , WardId , WardCode , WardName , VillageId , VillageCode , VillageName , ParentLocationId  FROM uvwLocations WHERE DistrictId = @DistrictId or RegionId = @RegionId  or @RegionId is NULL ;
+		DistrictName , ParentLocationId)( SELECT ISNULL(d.LocationId,r.LocationId) LocationId , r.LocationId as RegionId , r.LocationCode as RegionCode  , r.LocationName as RegionName , d.LocationId as DistrictId , d.LocationCode as DistrictCode , 
+		r.LocationName as DistrictName , ISNULL(d.ParentLocationId,r.ParentLocationId) ParentLocationId FROM @Locations  d  INNER JOIN @Locations r on d.ParentLocationId = r.LocationId
+		UNION ALL SELECT r.LocationId, r.LocationId as RegionId , r.LocationCode as RegionCode  , r.LocationName as RegionName , NULL DistrictId , NULL DistrictCode , 
+		NULL DistrictName ,  ParentLocationId FROM @Locations  r WHERE ParentLocationId = 0)
+		;
 		declare @listOfHF table (id int);
 		
-		IF  @RegionId IS  NULL 
+		IF  @RegionId IS  NULL or @RegionId =0
 			INSERT INTO @listOfHF(id) SELECT tblHF.HfID FROM tblHF WHERE tblHF.ValidityTo is NULL;
-		 ELSE IF  @DistrictId is NULL
-			INSERT INTO @listOfHF(id) SELECT tblHF.HfID FROM tblHF JOIN tblLocations l on tblHF.LocationId = l. LocationId  WHERE l.ParentLocationId =  @RegionId  and tblHF.ValidityTo is NULL;
+		 ELSE IF  @DistrictId is NULL or @DistrictId =0
+			INSERT INTO @listOfHF(id) SELECT tblHF.HfID FROM tblHF JOIN tblLocations l on tblHF.LocationId = l.LocationId   WHERE l.ParentLocationId =  @RegionId  ;
 		ELSE 
 			INSERT INTO @listOfHF(id) SELECT tblHF.HfID FROM tblHF WHERE tblHF.LocationId = @DistrictId and tblHF.ValidityTo is NULL;
 
@@ -1420,18 +1451,17 @@ BEGIN
 		INSERT INTO @TotalPopFam 
 	    
 		    SELECT C.HFID HFID ,
-		    CASE WHEN ISNULL(@DistrictId, @RegionId) IN (R.RegionId, D.DistrictId) THEN 1 ELSE 0 END * SUM((ISNULL(L.MalePopulation, 0) + ISNULL(L.FemalePopulation, 0) + ISNULL(L.OtherPopulation, 0)) *(0.01* Catchment)) TotalPopulation, 
-		    CASE WHEN ISNULL(@DistrictId, @RegionId) IN (R.RegionId, D.DistrictId) THEN 1 ELSE 0 END * SUM(ISNULL(((L.Families)*(0.01* Catchment)), 0))TotalFamilies
+		    CASE WHEN ISNULL(@DistrictId, @RegionId) IN (D.RegionId, D.DistrictId) THEN 1 ELSE 0 END * SUM((ISNULL(L.MalePopulation, 0) + ISNULL(L.FemalePopulation, 0) + ISNULL(L.OtherPopulation, 0)) *(0.01* Catchment)) TotalPopulation, 
+		    CASE WHEN ISNULL(@DistrictId, @RegionId) IN (D.RegionId, D.DistrictId) THEN 1 ELSE 0 END * SUM(ISNULL(((L.Families)*(0.01* Catchment)), 0))TotalFamilies
 		    FROM tblHFCatchment C
-		    LEFT JOIN tblLocations L ON L.LocationId = C.LocationId
+		    LEFT JOIN tblLocations L ON L.LocationId = C.LocationId OR  L.LegacyId = C.LocationId
 		    INNER JOIN tblHF HF ON C.HFID = HF.HfID
-		    INNER JOIN tblDistricts D ON HF.LocationId = D.DistrictId
-		    INNER JOIN tblRegions R ON D.Region = R.RegionId
-		    WHERE C.ValidityTo IS NULL
-		    AND L.ValidityTo IS NULL
-		    AND HF.ValidityTo IS NULL
+		    INNER JOIN @LocationTemp D ON HF.LocationId = D.DistrictId
+		    WHERE (C.ValidityTo IS NULL OR C.ValidityTo >= @FirstDay) AND C.ValidityFrom< @FirstDay
+		    AND(L.ValidityTo IS NULL OR L.ValidityTo >= @FirstDay) AND L.ValidityFrom< @FirstDay
+		    AND (HF.ValidityTo IS NULL )
 			AND C.HFID in  (SELECT id FROM @listOfHF)
-		    GROUP BY C.HFID, D.DistrictId, R.RegionId
+		    GROUP BY C.HFID, D.DistrictId, D.RegionId
 	    
 
 
@@ -1449,7 +1479,7 @@ BEGIN
 		    INNER JOIN tblFamilies F ON F.FamilyId = I.FamilyId
 		    INNER JOIN tblHFCatchment HC ON HC.LocationId = F.LocationId
 		    INNER JOIN tblPolicy PL ON PL.PolicyID = IP.PolicyId
-		    WHERE HC.ValidityTo IS NULL 
+		    WHERE (HC.ValidityTo IS NULL OR HC.ValidityTo >= @FirstDay) AND HC.ValidityFrom< @FirstDay
 		    AND I.ValidityTo IS NULL
 		    AND IP.ValidityTo IS NULL
 		    AND F.ValidityTo IS NULL
@@ -1475,7 +1505,7 @@ BEGIN
 		    INNER JOIN tblFamilies F ON F.InsureeID = I.InsureeID
 		    INNER JOIN tblHFCatchment HC ON HC.LocationId = F.LocationId
 		    INNER JOIN tblPolicy PL ON PL.PolicyID = IP.PolicyId
-		    WHERE HC.ValidityTo IS NULL 
+		    WHERE (HC.ValidityTo IS NULL OR HC.ValidityTo >= @FirstDay) AND HC.ValidityFrom< @FirstDay
 		    AND I.ValidityTo IS NULL
 		    AND IP.ValidityTo IS NULL
 		    AND F.ValidityTo IS NULL
@@ -1488,56 +1518,8 @@ BEGIN
 
 
 
-		DECLARE @ClaimValues TABLE (
-			HFID INT,
-			ProdId INT,
-			TotalAdjusted DECIMAL(18, 6),
-			TotalClaims DECIMAL(18, 6)
-			);
 
-		INSERT INTO @ClaimValues
-		SELECT HFID, @ProdId ProdId, SUM(PriceValuated)TotalAdjusted, COUNT(DISTINCT  ClaimId)TotalClaims
-		FROM(
-		SELECT C.HFID, CValue.PriceValuated, C.ClaimId
-		FROM tblClaim C WITH (NOLOCK)
-		INNER JOIN (
-			SELECT ClaimId, PriceValuated FROM tblClaimItems WITH (NOLOCK) WHERE ValidityTo IS NULL AND ProdId = @ProdId
-			UNION ALL
-			SELECT ClaimId, PriceValuated FROM tblClaimServices WITH (NOLOCK) WHERE ValidityTo IS NULL AND ProdId = @ProdId
-			) CValue ON CValue.ClaimID = C.ClaimID
-		WHERE (@WeightAdjustedAmount > 0.0) 
-		AND C.ValidityTo IS NULL
-		AND C.ClaimStatus >= 8
-		AND YEAR(C.DateProcessed) = @Year
-		AND MONTH(C.DateProcessed) = @Month
-		AND C.HFID  in  (SELECT id FROM @listOfHF)
-		)CValue 
-		GROUP BY HFID 
-
-		DECLARE @Locations TABLE (
-			LocationId INT,
-			LocationName VARCHAR(MAX),
-			ParentLocationId INT
-			);
-	    
-		INSERT INTO @Locations 
-		    SELECT 0 LocationId, N'National' LocationName, NULL ParentLocationId
-		    
-			UNION ALL
-		    
-			SELECT LocationId,LocationName, ISNULL(ParentLocationId, 0) 
-			FROM tblLocations 
-			WHERE ValidityTo IS NULL 
-				AND (LocationId = ISNULL(@DistrictId, @RegionId) OR 
-				(LocationType IN ('R', 'D') AND ParentLocationId = ISNULL(@DistrictId, @RegionId)))
-		    
-			/*UNION ALL
-		    
-			SELECT L.LocationId, L.LocationName, L.ParentLocationId 
-		    FROM tblLocations L 
-		    INNER JOIN @Locations LC ON @LC.LocationId = L.ParentLocationId
-		    WHERE L.validityTo IS NULL
-		    AND L.LocationType IN ('R', 'D')*/
+		
 	    
 		DECLARE @Allocation TABLE (
 			ProdId INT,
@@ -1559,14 +1541,14 @@ BEGIN
 		    FROM tblPremium PR 
 		    INNER JOIN tblPolicy PL ON PR.PolicyID = PL.PolicyID
 		    INNER JOIN tblProduct Prod ON Prod.ProdId = PL.ProdID
-		    INNER JOIN @Locations L ON ISNULL(Prod.LocationId, 0) = L.LocationId
+		    INNER JOIN  @Locations L ON ISNULL(Prod.LocationId, 0) = L.LocationId
 		    WHERE PR.ValidityTo IS NULL
 		    AND PL.ValidityTo IS NULL
 		    AND PL.ProdID = @ProdId
 		    AND PL.PolicyStatus <> 1
 		    AND PR.PayDate <= PL.ExpiryDate
 		    GROUP BY PL.ProdID, PL.ExpiryDate, PR.PayDate,PL.EffectiveDate)Alc
-		    GROUP BY ProdId
+		    GROUP BY ProdId;
 	    
 
 		DECLARE @ReportData TABLE (
@@ -1603,6 +1585,35 @@ BEGIN
 
 			);
 	    
+		DECLARE @ClaimValues TABLE (
+			HFID INT,
+			ProdId INT,
+			TotalAdjusted DECIMAL(18, 6),
+			TotalClaims DECIMAL(18, 6)
+			);
+
+		INSERT INTO @ClaimValues
+		SELECT HFID, @ProdId ProdId, SUM(TotalAdjusted)TotalAdjusted, COUNT(DISTINCT ClaimId)TotalClaims FROM
+		(
+			SELECT HFID, SUM(PriceValuated)TotalAdjusted, ClaimId
+			FROM 
+			(SELECT HFID,c.ClaimId, PriceValuated FROM  tblClaim C WITH (NOLOCK)
+			 LEFT JOIN tblClaimItems ci ON c.ClaimID = ci.ClaimID and  ProdId = @ProdId AND (@WeightAdjustedAmount > 0.0)
+			 WHERE CI.ValidityTo IS NULL  AND C.ValidityTo IS NULL
+				AND C.ClaimStatus > 4
+				AND YEAR(C.DateProcessed) = @Year
+				AND MONTH(C.DateProcessed) = @Month
+				AND C.HFID  in  (SELECT id FROM @listOfHF)and ci.ValidityTo IS NULL 
+			UNION ALL
+			SELECT HFID, c.ClaimId, PriceValuated FROM tblClaim C WITH (NOLOCK) 
+			LEFT JOIN tblClaimServices cs ON c.ClaimID = cs.ClaimID   and  ProdId = @ProdId AND (@WeightAdjustedAmount > 0.0)
+			WHERE cs.ValidityTo IS NULL  	AND C.ValidityTo IS NULL
+				AND C.ClaimStatus > 4
+				AND YEAR(C.DateProcessed) = @Year
+				AND MONTH(C.DateProcessed) = @Month	
+				AND C.HFID  in (SELECT id FROM @listOfHF) and CS.ValidityTo IS NULL 
+			) claimdetails GROUP BY HFID,ClaimId
+		)claims GROUP by HFID
 
 	    INSERT INTO @ReportData 
 		    SELECT L.RegionCode, L.RegionName, L.DistrictCode, L.DistrictName, HF.HFCode, HF.HFName, Hf.AccCode, 
@@ -1652,7 +1663,7 @@ BEGIN
 			    OR ((HF.HFLevel = @Level2 ) AND (HF.HFSublevel = @Sublevel2 OR @Sublevel2 IS NULL))
 			    OR ((HF.HFLevel = @Level3) AND (HF.HFSublevel = @Sublevel3 OR @Sublevel3 IS NULL))
 			    OR ((HF.HFLevel = @Level4) AND (HF.HFSublevel = @Sublevel4 OR @Sublevel4 IS NULL))
-		      )
+		      );
 
 
 
@@ -1693,7 +1704,14 @@ BEGIN
 END
 GO
 
-CREATE NONCLUSTERED INDEX NCI_HF_ValidityTo ON tblHF(ValidityTo)
+BEGIN TRY
+	BEGIN TRANSACTION; 
+	CREATE NONCLUSTERED INDEX NCI_HF_ValidityTo ON tblHF(ValidityTo)
+	COMMIT TRANSACTION;  
+END TRY
+BEGIN CATCH  
+     ROLLBACK  TRANSACTION;  
+END CATCH  
 
 -- OP-154: database partitioning  
 -- moved from migration 1.4.2 
@@ -1701,17 +1719,31 @@ CREATE NONCLUSTERED INDEX NCI_HF_ValidityTo ON tblHF(ValidityTo)
 -- Adds four new filegroups to the database  
 
 -- if the DATETIME provided in before 1970 then it goes to partition 1 else it goes to partition 2
-CREATE PARTITION FUNCTION [StillValid] (DATETIME) AS RANGE LEFT
-FOR
-VALUES (
-	N'1970-01-01T00:00:00.001'
+BEGIN TRY
+	BEGIN TRANSACTION; 
+	CREATE PARTITION FUNCTION [StillValid] (DATETIME) AS RANGE LEFT
+	FOR
+	VALUES (
+		N'1970-01-01T00:00:00.001'
 	)
+	COMMIT TRANSACTION;  
+END TRY
+BEGIN CATCH  
+     ROLLBACK  TRANSACTION;  
+END CATCH  
 
 -- Create partition Scheme that will define the partition to be used, both use the PRIMARY file group (not IDEAL but done to limit changes in a crisis mode)
-CREATE PARTITION SCHEME [liveArchive] AS PARTITION [StillValid] TO (
-	[PRIMARY]
-	,[PRIMARY]
-)
+BEGIN TRY
+	BEGIN TRANSACTION; 
+	CREATE PARTITION SCHEME [liveArchive] AS PARTITION [StillValid] TO (
+		[PRIMARY]
+		,[PRIMARY]
+	)
+	COMMIT TRANSACTION;  
+END TRY
+BEGIN CATCH  
+     ROLLBACK  TRANSACTION;  
+END CATCH  
 
 BEGIN TRY
 	BEGIN TRANSACTION; 
@@ -1836,7 +1868,12 @@ BEGIN TRY
 	ALTER TABLE tblFamilies DROP CONSTRAINT FK_tblFamilies_tblLocations
 	ALTER TABLE [tblLocations] DROP CONSTRAINT [PK_tblLocations]
 
-	CREATE UNIQUE CLUSTERED INDEX CI_tblLocations ON tblLocations (		[ValidityTo] ASC,		[LocationId] ASC,		[LocationCode] ASC,		[LocationName] ASC,		[LocationType] ASC
+	CREATE UNIQUE CLUSTERED INDEX CI_tblLocations ON tblLocations (
+		[ValidityTo] ASC,
+		[LocationId] ASC,
+		[LocationCode] ASC,
+		[LocationName] ASC,
+		[LocationType] ASC
 		)
 	WITH
 		( PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, 

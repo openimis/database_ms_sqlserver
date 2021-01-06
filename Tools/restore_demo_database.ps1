72e3b2e6426c61fd9abf7a82b4f3e6a41f9e9050
Dragos Dobre <dragos.dobre@swisstph.ch>
2021-01-06 17:40:41 +0100

    add restore_demo_database fix uspProcessSingleClaimStep2

---
+++ Tools/restore_demo_database.ps1
@@ -0,0 +1,15 @@
+$DATABASE = "e1"
+$USERNAME = "sa"
+$PASSWORD = "+Q2w3e4r5"
+$OPENIMIS_DATABASE_GIT_FOLDER = ".."
+#remove old database
+sqlcmd -S 127.0.0.1 -U "$USERNAME" -P "$PASSWORD" -Q "drop database $DATABASE"
+#create new database
+sqlcmd -S 127.0.0.1 -U "$USERNAME" -P "$PASSWORD" -Q "create database $DATABASE"
+#create the structure 
+sqlcmd -S 127.0.0.1 -U "$USERNAME" -P "$PASSWORD" -d "$DATABASE" -i "$OPENIMIS_DATABASE_GIT_FOLDER\Empty databases\openIMIS_ONLINE.sql"
+#add demo dataset 
+sqlcmd -S 127.0.0.1 -U "$USERNAME" -P "$PASSWORD" -d "$DATABASE" -i "$OPENIMIS_DATABASE_GIT_FOLDER\Demo database\openIMIS_demo_ONLINE.sql"
+
+
+

fc8fdd88a69a1f2db76fb34e335d5801c8e5f777
Dragos Dobre <dragos.dobre@swisstph.ch>
2021-03-15 14:09:13 +0100

    add payment SMS

--- Migration script/openIMIS migration latest.sql
+++ Migration script/openIMIS migration latest.sql
@@ -81,6 +81,24 @@ BEGIN
 END 
 GO
 
+IF COL_LENGTH('tblPayment', 'SmsRequired') IS NULL
+BEGIN
+	ALTER TABLE tblPayment ADD [SmsRequired] [bit] NULL
+END 
+GO
+
+if not exists (
+    select *
+      from sys.all_columns c
+      join sys.tables t on t.object_id = c.object_id
+      join sys.schemas s on s.schema_id = t.schema_id
+      join sys.default_constraints d on c.default_object_id = d.object_id
+    where t.name = 'tblPayment'
+      and c.name = 'PaymentUUID'
+      and s.name = 'dbo')
+ALTER TABLE [dbo].[tblPayment] ADD DEFAULT (newid()) FOR PaymentUUID
+GO
+
 ALTER VIEW [dbo].[uvwLocations]
 AS
 	SELECT 0 LocationId, null VillageId, null VillageName,NULL VillageCode, null WardId, null WardName,NULL WardCode, null DistrictId,null DistrictName, NULL DistrictCode, NULL RegionId, N'National' RegionName, null RegionCode, 0 ParentLocationId
@@ -4451,7 +4469,7 @@ BEGIN TRY
 		--DISTRIBUTE PAYMENTS 
 		DECLARE @curPaymentID int, @ReceivedAmount DECIMAL(18,2), @TotalPolicyValue DECIMAL(18,2), @AmountAvailable DECIMAL(18,2)
 		DECLARE CUR_Pay CURSOR FAST_FORWARD FOR
-		SELECT PH.PaymentID, ReceivedAmount, TotalPolicyValue FROM @tblHeader PH
+		SELECT PH.PaymentID, ReceivedAmount FROM @tblHeader PH
 		OPEN CUR_Pay
 		FETCH NEXT FROM CUR_Pay INTO  @curPaymentID, @ReceivedAmount
 		WHILE @@FETCH_STATUS = 0
@@ -5021,5 +5039,339 @@ END TRY
 	END CATCH
 	
 END
+GO
 
 SET ANSI_NULLS ON
+
+
+IF OBJECT_ID('[uspInsertPaymentIntent]', 'P') IS NOT NULL
+    DROP PROCEDURE [uspInsertPaymentIntent]
+GO
+CREATE PROCEDURE [dbo].[uspInsertPaymentIntent]
+(
+	@XML XML,
+	@ExpectedAmount DECIMAL(18,2) = 0 OUT, 
+	@PaymentID INT = 0 OUT,
+	@ErrorNumber INT = 0,
+	@ErrorMsg NVARCHAR(255)=NULL,
+	@ProvidedAmount decimal(18,2) = 0,
+	@PriorEnrollment BIT = 0 
+	
+)
+
+AS
+BEGIN
+	DECLARE @tblHeader TABLE(officerCode nvarchar(12),requestDate DATE, phoneNumber NVARCHAR(50),LanguageName NVARCHAR(10),SmsRequired BIT, AuditUSerID INT)
+	DECLARE @tblDetail TABLE(InsuranceNumber nvarchar(12),productCode nvarchar(8), PolicyStage NVARCHAR(1),  isRenewal BIT, PolicyValue DECIMAL(18,2), isExisting BIT)
+	DECLARE @OfficerLocationID INT
+	DECLARE @OfficerParentLocationID INT
+	DECLARE @AdultMembers INT 
+	DECLARE @ChildMembers INT 
+	DECLARE @oAdultMembers INT 
+	DECLARE @oChildMembers INT
+
+
+	DECLARE @isEO BIT
+		INSERT INTO @tblHeader(officerCode, requestDate, phoneNumber,LanguageName, SmsRequired, AuditUSerID)
+		SELECT 
+		LEFT(NULLIF(T.H.value('(OfficerCode)[1]','NVARCHAR(50)'),''),12),
+		NULLIF(T.H.value('(RequestDate)[1]','NVARCHAR(50)'),''),
+		LEFT(NULLIF(T.H.value('(PhoneNumber)[1]','NVARCHAR(50)'),''),50),
+		NULLIF(T.H.value('(LanguageName)[1]','NVARCHAR(10)'),''),
+		T.H.value('(SmsRequired)[1]','BIT'),
+		NULLIF(T.H.value('(AuditUserId)[1]','INT'),'')
+		FROM @XML.nodes('PaymentIntent/Header') AS T(H)
+
+		INSERT INTO @tblDetail(InsuranceNumber, productCode, isRenewal)
+		SELECT 
+		LEFT(NULLIF(T.D.value('(InsuranceNumber)[1]','NVARCHAR(12)'),''),12),
+		LEFT(NULLIF(T.D.value('(ProductCode)[1]','NVARCHAR(8)'),''),8),
+		T.D.value('(IsRenewal)[1]','BIT')
+		FROM @XML.nodes('PaymentIntent/Details/Detail') AS T(D)
+		
+		IF @ErrorNumber != 0
+		BEGIN
+			GOTO Start_Transaction;
+		END
+
+		SELECT @AdultMembers =T.P.value('(AdultMembers)[1]','INT'), @ChildMembers = T.P.value('(ChildMembers)[1]','INT') , @oAdultMembers =T.P.value('(oAdultMembers)[1]','INT') , @oChildMembers = T.P.value('(oChildMembers)[1]','INT') FROM @XML.nodes('PaymentIntent/ProxySettings') AS T(P)
+		
+		SELECT @AdultMembers= ISNULL(@AdultMembers,0), @ChildMembers= ISNULL(@ChildMembers,0), @oAdultMembers= ISNULL(@oAdultMembers,0), @oChildMembers= ISNULL(@oChildMembers,0)
+		
+
+		UPDATE D SET D.isExisting = 1 FROM @tblDetail D 
+		INNER JOIN  tblInsuree I ON D.InsuranceNumber = I.CHFID 
+		WHERE I.IsHead = 1 AND I.ValidityTo IS NULL
+	
+		UPDATE  @tblDetail SET isExisting = 0 WHERE isExisting IS NULL
+
+		IF EXISTS(SELECT 1 FROM @tblHeader WHERE officerCode IS NOT NULL)
+			SET @isEO = 1
+	/**********************************************************************************************************************
+			VALIDATION STARTS
+	*********************************************************************************************************************/
+	/*Error Codes
+	2- Not valid insurance or missing product code
+	3- Not valid enrolment officer code
+	4 –Enrolment officer code and insurance product code are not compatible
+	5- Beneficiary has no policy of specified insurance product for renewal
+	6- Can not issue a control number as default indicated prior enrollment and Insuree has not been enrolled yet 
+
+	7 - 'Insuree not enrolled and prior enrolement mandatory'
+
+
+	-1. Unexpected error
+	0. Success
+	*/
+
+	--2. Insurance number missing
+		IF EXISTS(SELECT 1 FROM @tblDetail WHERE LEN(ISNULL(InsuranceNumber,'')) =0)
+		BEGIN
+			SET @ErrorNumber = 2
+			SET @ErrorMsg ='Not valid insurance or missing product code'
+			GOTO Start_Transaction;
+		END
+
+		--4. Missing product or Product does not exists
+		IF EXISTS(SELECT 1 FROM @tblDetail D 
+				LEFT OUTER JOIN tblProduct P ON P.ProductCode = D.productCode
+				WHERE 
+				(P.ValidityTo IS NULL AND P.ProductCode IS NULL) 
+				OR D.productCode IS NULL
+				)
+		BEGIN
+			SET @ErrorNumber = 4
+			SET @ErrorMsg ='Not valid insurance or missing product code'
+			GOTO Start_Transaction;
+		END
+
+
+	--3. Invalid Officer Code
+		IF EXISTS(SELECT 1 FROM @tblHeader H
+				LEFT JOIN tblOfficer O ON H.officerCode = O.Code
+				WHERE O.ValidityTo IS NULL
+				AND H.officerCode IS NOT NULL
+				AND O.Code IS NULL
+		)
+		BEGIN
+			SET @ErrorNumber = 3
+			SET @ErrorMsg ='Not valid enrolment officer code'
+			GOTO Start_Transaction;
+		END
+
+		
+		--4. Wrong match of Enrollment Officer agaists Product
+		SELECT @OfficerLocationID= L.LocationId, @OfficerParentLocationID = L.ParentLocationId FROM tblLocations L 
+		INNER JOIN tblOfficer O ON O.LocationId = L.LocationId AND O.Code = (SELECT officerCode FROM @tblHeader WHERE officerCode IS NOT NULL)
+		WHERE 
+		L.ValidityTo IS NULL
+		AND O.ValidityTo IS NULL
+
+
+		IF EXISTS(SELECT D.productCode, P.ProductCode FROM @tblDetail D
+			LEFT OUTER JOIN tblProduct P ON P.ProductCode = D.productCode AND (P.LocationId IS NULL OR P.LocationId = @OfficerLocationID OR P.LocationId = @OfficerParentLocationID)
+			WHERE
+			P.ValidityTo IS NULL
+			AND P.ProductCode IS NULL
+			) AND EXISTS(SELECT 1 FROM @tblHeader WHERE officerCode IS NOT NULL)
+		BEGIN
+			SET @ErrorNumber = 4
+			SET @ErrorMsg ='Enrolment officer code and insurance product code are not compatible'
+			GOTO Start_Transaction;
+		END
+		
+		
+		--The family does't contain this product for renewal
+		IF EXISTS(SELECT 1 FROM @tblDetail D
+				LEFT OUTER JOIN tblProduct PR ON PR.ProductCode = D.productCode
+				LEFT OUTER JOIN tblInsuree I ON I.CHFID = D.InsuranceNumber
+				LEFT OUTER JOIN tblPolicy PL ON PL.FamilyID = I.FamilyID  AND PL.ProdID = PR.ProdID
+				WHERE PR.ValidityTo IS NULL
+				AND D.isRenewal = 1 AND D.isExisting = 1
+				AND I.ValidityTo IS NULL
+				AND PL.ValidityTo IS NULL AND PL.PolicyID IS NULL)
+		BEGIN
+			SET @ErrorNumber = 5
+			SET @ErrorMsg ='Beneficiary has no policy of specified insurance product for renewal'
+			GOTO Start_Transaction;
+		END
+
+		
+
+		--5. Proxy family can not renew
+		IF EXISTS(SELECT 1 FROM @tblDetail WHERE isExisting =0 AND isRenewal= 1)
+		BEGIN
+			SET @ErrorNumber = 5
+			SET @ErrorMsg ='Beneficiary has no policy of specified insurance product for renewal'
+			GOTO Start_Transaction;
+		END
+
+
+		--7. Insurance number not existing in system 
+		IF @PriorEnrollment = 1 AND EXISTS(SELECT 1 FROM @tblDetail D
+				LEFT OUTER JOIN tblProduct PR ON PR.ProductCode = D.productCode
+				LEFT OUTER JOIN tblInsuree I ON I.CHFID = D.InsuranceNumber
+				LEFT OUTER JOIN tblPolicy PL ON PL.FamilyID = I.FamilyID  AND PL.ProdID = PR.ProdID
+				WHERE PR.ValidityTo IS NULL
+				AND I.ValidityTo IS NULL
+				AND PL.ValidityTo IS NULL AND PL.PolicyID IS NULL)
+		BEGIN
+			SET @ErrorNumber = 7
+			SET @ErrorMsg ='Insuree not enrolled and prior enrollment mandatory'
+			GOTO Start_Transaction;
+		END
+
+
+
+
+	/**********************************************************************************************************************
+			VALIDATION ENDS
+	*********************************************************************************************************************/
+	/**********************************************************************************************************************
+			CALCULATIONS STARTS
+	*********************************************************************************************************************/
+	
+	DECLARE @FamilyId INT, @ProductId INT, @PrevPolicyID INT, @PolicyID INT, @PremiumID INT
+	DECLARE @PolicyStatus TINYINT=1
+	DECLARE @PolicyValue DECIMAL(18,2), @PolicyStage NVARCHAR(1), @InsuranceNumber nvarchar(12), @productCode nvarchar(8), @enrollmentDate DATE, @isRenewal BIT, @isExisting BIT, @ErrorCode NVARCHAR(50)
+
+	IF @ProvidedAmount = 0 
+	BEGIN
+		--only calcuylate if we want the system to provide a value to settle
+		DECLARE CurFamily CURSOR FOR SELECT InsuranceNumber, productCode, isRenewal, isExisting FROM @tblDetail
+		OPEN CurFamily
+		FETCH NEXT FROM CurFamily INTO @InsuranceNumber, @productCode, @isRenewal, @isExisting;
+		WHILE @@FETCH_STATUS = 0
+		BEGIN
+									
+			SET @PolicyStatus=1
+			SET @FamilyId = NULL
+			SET @ProductId = NULL
+			SET @PolicyID = NULL
+
+
+			IF @isRenewal = 1
+				SET @PolicyStage = 'R'
+			ELSE 
+				SET @PolicyStage ='N'
+
+			IF @isExisting = 1
+			BEGIN
+											
+				SELECT @FamilyId = FamilyId FROM tblInsuree I WHERE IsHead = 1 AND CHFID = @InsuranceNumber  AND ValidityTo IS NULL
+				SELECT @ProductId = ProdID FROM tblProduct WHERE ProductCode = @productCode  AND ValidityTo IS NULL
+				SELECT TOP 1 @PolicyID =  PolicyID FROM tblPolicy WHERE FamilyID = @FamilyId  AND ProdID = @ProductId AND PolicyStage = @PolicyStage AND ValidityTo IS NULL ORDER BY EnrollDate DESC
+											
+				IF @isEO = 1
+					IF EXISTS(SELECT 1 FROM tblPremium WHERE PolicyID = @PolicyID AND ValidityTo IS NULL)
+					BEGIN
+						SELECT @PolicyValue =  ISNULL(PR.Amount - ISNULL(MatchedAmount,0),0) FROM tblPremium PR
+														INNER JOIN tblPolicy PL ON PL.PolicyID = PR.PolicyID
+														LEFT OUTER JOIN (SELECT PremiumID, SUM (Amount) MatchedAmount from tblPaymentDetails WHERE ValidityTo IS NULL GROUP BY PremiumID ) PD ON PD.PremiumID = PR.PremiumId
+														WHERE
+														PR.ValidityTo IS NULL
+														AND PL.ValidityTo IS NULL AND PL.PolicyID = @PolicyID
+														IF @PolicyValue < 0
+														SET @PolicyValue = 0.00
+					END
+					ELSE
+					BEGIN
+						EXEC @PolicyValue = uspPolicyValue @FamilyId, @ProductId, 0, @PolicyStage, NULL, 0;
+					END
+												
+				ELSE IF @PolicyStage ='N'
+				BEGIN
+					EXEC @PolicyValue = uspPolicyValue @FamilyId, @ProductId, 0, 'N', NULL, 0;
+				END
+				ELSE
+				BEGIN
+					SELECT TOP 1 @PrevPolicyID = PolicyID, @PolicyStatus = PolicyStatus FROM tblPolicy  WHERE ProdID = @ProductId AND FamilyID = @FamilyId AND ValidityTo IS NULL AND PolicyStatus != 4 ORDER BY EnrollDate DESC
+					IF @PolicyStatus = 1
+					BEGIN
+						SELECT @PolicyValue =  (ISNULL(SUM(PL.PolicyValue),0) - ISNULL(SUM(Amount),0)) FROM tblPolicy PL 
+						LEFT OUTER JOIN tblPremium PR ON PR.PolicyID = PL.PolicyID
+						WHERE PL.ValidityTo IS NULL
+						AND PR.ValidityTo IS NULL
+						AND PL.PolicyID = @PrevPolicyID
+						IF @PolicyValue < 0
+							SET @PolicyValue =0
+					END
+					ELSE
+					BEGIN 
+						EXEC @PolicyValue = uspPolicyValue @FamilyId, @ProductId, 0, 'R', @enrollmentDate, @PrevPolicyID, @ErrorCode OUTPUT;
+					END
+				END
+			END
+			ELSE
+			BEGIN
+				EXEC @PolicyValue = uspPolicyValueProxyFamily @productCode, @AdultMembers, @ChildMembers,@oAdultMembers,@oChildMembers
+			END
+			UPDATE @tblDetail SET PolicyValue = ISNULL(@PolicyValue,0), PolicyStage = @PolicyStage  WHERE InsuranceNumber = @InsuranceNumber AND productCode = @productCode AND isRenewal = @isRenewal
+			FETCH NEXT FROM CurFamily INTO @InsuranceNumber, @productCode, @isRenewal, @isExisting;
+		END
+		CLOSE CurFamily
+		DEALLOCATE CurFamily;
+
+	
+
+	END
+
+	
+	
+		
+	--IF IT REACHES UP TO THIS POINT THEN THERE IS NO ERROR
+	SET @ErrorNumber = 0
+	SET @ErrorMsg = NULL
+
+
+	/**********************************************************************************************************************
+			CALCULATIONS ENDS
+	 *********************************************************************************************************************/
+
+	 /**********************************************************************************************************************
+			INSERTION STARTS
+	 *********************************************************************************************************************/
+		Start_Transaction:
+		BEGIN TRY
+			BEGIN TRANSACTION INSERTPAYMENTINTENT
+				
+				IF @ProvidedAmount > 0 
+					SET @ExpectedAmount = @ProvidedAmount 
+				ELSE
+					SELECT @ExpectedAmount = SUM(ISNULL(PolicyValue,0)) FROM @tblDetail
+				
+				SET @ErrorMsg = ISNULL(CONVERT(NVARCHAR(5),@ErrorNumber)+': '+ @ErrorMsg,NULL)
+				--Inserting Payment
+				INSERT INTO [dbo].[tblPayment]
+				 ([ExpectedAmount],[OfficerCode],[PhoneNumber],[RequestDate],[PaymentStatus],[ValidityFrom],[AuditedUSerID],[RejectedReason],[LanguageName],[SmsRequired]) 
+				 SELECT
+				 @ExpectedAmount, officerCode, phoneNumber, GETDATE(),CASE @ErrorNumber WHEN 0 THEN 0 ELSE -1 END, GETDATE(), AuditUSerID, @ErrorMsg,LanguageName,SmsRequired
+				 FROM @tblHeader
+				 SELECT @PaymentID= SCOPE_IDENTITY();
+
+				 --Inserting Payment Details
+				 DECLARE @AuditedUSerID INT
+				 SELECT @AuditedUSerID = AuditUSerID FROM @tblHeader
+				INSERT INTO [dbo].[tblPaymentDetails]
+			   ([PaymentID],[ProductCode],[InsuranceNumber],[PolicyStage],[ValidityFrom],[AuditedUserId], ExpectedAmount) SELECT
+				@PaymentID, productCode, InsuranceNumber,  CASE isRenewal WHEN 0 THEN 'N' ELSE 'R' END, GETDATE(), @AuditedUSerID, PolicyValue
+				FROM @tblDetail D
+				
+
+
+			COMMIT TRANSACTION INSERTPAYMENTINTENT
+			RETURN @ErrorNumber
+		END TRY
+		BEGIN CATCH
+			ROLLBACK TRANSACTION INSERTPAYMENTINTENT
+			SELECT ERROR_MESSAGE()
+			RETURN -1
+		END CATCH
+
+	 /**********************************************************************************************************************
+			INSERTION ENDS
+	 *********************************************************************************************************************/
+
+END
+GO
+
